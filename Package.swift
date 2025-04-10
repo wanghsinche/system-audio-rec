6b7d4cf8969b51c910daec7dce3e966fa9775552
hendy <wanghsinche>
2025-04-11 01:11:09 +0800

    fix: remove it

--- Package.swift
+++ Package.swift
@@ -6,20 +6,16 @@ import PackageDescription
 let package = Package(
     name: "ScreenCaptureTool",
     platforms: [
-        .macOS(.v12) // Specify minimum macOS version for ScreenCaptureKit
+        .macOS(.v12_3) // ScreenCaptureKit requires macOS 12.3+
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/kPherox/ScreenCaptureKit", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "ScreenCaptureTool",
-            dependencies: ["ScreenCaptureKit"],
-            linkerSettings: [
-                .unsafeFlags(["-Xlinker", "-embed-bitcode"])
-            ]),
+            dependencies: []),
     ]
 )
